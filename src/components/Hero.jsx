937e40b4346d1a5d4afc1ba33be06b8ec9477011
alb1nut <albertnutifafa@gmail.com>
2023-07-21 03:27:21 +0000

    hero section description section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,17 +10,17 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+          <div className='w-5 h-5 rounded-full bg-[#7CFC00]' />
+          <div className='w-1 sm:h-80 h-40 bg-gradient-to-t from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#7CFC00]'>Albert</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+          I'm a web and mobile developer who transforms <br className='sm:block hidden' />
+          ideas into reality. 
           </p>
         </div>
       </div>
