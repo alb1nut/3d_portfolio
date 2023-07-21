0f116eea63488b53ee43bb780e0d5c0d9271d6cb
alb1nut <albertnutifafa@gmail.com>
2023-07-21 03:06:14 +0000

    changes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
+import { logobgwhite, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -42,10 +42,10 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logobgwhite} alt='logo' className='w-16 h-16 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Albert &nbsp;
+            <span className='sm:block hidden'> Nutifafa Dovlo</span>
           </p>
         </Link>
 
